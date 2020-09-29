5174188e1ca9441404c4cf0b3bfb0189b3d0b4d4
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-09-29 18:28:01 -0300

    initial commit

---
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.contador;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int contador = 0;
+    static final String CONTAGEM = "contagem";
+
+
+    public void atualizarTextoBotao() {
+        Button meuBotao = findViewById(R.id.botaoContador);
+        String textoBotao = getResources().getString(R.string.status_contador);
+        meuBotao.setText(textoBotao + " " + contador);
+    }
+
+
+    public void botaoContadorClick(View botao) {
+        contador++;
+        Log.i(this.getClass().getName(), "Dentro do botaoContadorClick " + contador);
+        atualizarTextoBotao();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            contador = savedInstanceState.getInt(CONTAGEM);
+            Log.i(this.getClass().getName(), "Dentro do onCreate com savedInstanceState =! null");
+        } else {
+            contador = 0;
+            Log.i(this.getClass().getName(), "Dentro do onCreate com savedInstanceState = null");
+        }
+        setContentView(R.layout.activity_main);
+        atualizarTextoBotao();
+
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        contador = savedInstanceState.getInt(CONTAGEM);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(this.getClass().getName(), "Dentro do onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(this.getClass().getName(), "Dentro do onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(this.getClass().getName(), "Dentro do onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(this.getClass().getName(), "Dentro do onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(this.getClass().getName(), "Dentro do onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(this.getClass().getName(), "Dentro do onRestart");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        Log.i(this.getClass().getName(), "Dentro do salvamento da instancia");
+        savedInstanceState.putInt(CONTAGEM, contador);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+}
